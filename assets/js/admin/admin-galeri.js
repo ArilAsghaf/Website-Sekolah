7f3c0e55e58cb49b13bdd9f0d81d3584a7230b2d
ArilAsghaf <you@example.comarilashgaf123@gmail.com>
2023-04-04 07:58:22 +0700

    Kelola Galeri

---
+++ assets/js/admin/admin-galeri.js
@@ -0,0 +1,230 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { doc, setDoc, getFirestore, getDoc, updateDoc, Timestamp, addDoc, collection, getDocs, deleteDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+import { getStorage, ref as refStorage, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyAc4tRSj1ENgI3oGU5ZUsAk684_fKyFVfU",
+	authDomain: "smpn-2-pati.firebaseapp.com",
+	projectId: "smpn-2-pati",
+	storageBucket: "smpn-2-pati.appspot.com",
+	messagingSenderId: "392087494788",
+	appId: "1:392087494788:web:924c46eb0a810c6067a2d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+
+
+// INPUT BERITA SEKOLAH
+const judulGaleri = document.querySelector(".judulGaleri");
+const btnGaleri = document.querySelector(".btnGaleri");
+const input_img = document.querySelector('.input-img');
+const editJudulGaleri = document.querySelector('.editModal')
+const editIsiGaleri = document.querySelector(".editTextarea")
+const simpanBtn = document.querySelector(".btnSimpan")
+var fileItem;
+var fileName;
+
+
+let dataInputAdmin = {
+	judul: "",
+	url_img: "",
+	tgl_uploud: + new Date()
+};
+
+async function getFile() {
+	fileItem = input_img.files[0];
+	fileName = fileItem.name;
+	const resp = await uploadImage(fileItem, fileName)
+	if (resp) {
+		const resp = await addGaleri(dataInputAdmin)
+		console.log(resp);
+		if (resp) {
+			Swal.fire({
+				icon: 'success',
+				title: 'Data berhasil disimpan',
+				showConfirmButton: false,
+				timer: 1500
+			})
+			setTimeout(() => {
+				location.reload()
+			}, 1610);
+		}
+	}
+}
+
+
+// UPLOAD IMAGE
+function uploadImage(file, name) {
+	return new Promise((resolve, reject) => {
+		const storage = getStorage(app);
+		const uploadTask = uploadBytesResumable(refStorage(storage, `gambar/${name}`), file)
+		uploadTask.on('state_changed',
+			(snapshot) => {
+				const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+				console.log(progress)
+				switch (snapshot.state) {
+					case 'paused':
+						console.log('Upload is paused');
+						break;
+					case 'running':
+						console.log('Upload is running');
+						break;
+				}
+			},
+			(error) => {
+				reject(error)
+			},
+			() => {
+				// Upload completed successfully, now we can get the download URL
+				getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+					dataInputAdmin = {
+						...dataInputAdmin,
+						url_img: downloadURL
+					}
+					resolve(true)
+				});
+			}
+		);
+	})
+}
+// END UPLOAD IMAGE
+
+
+// TIMESTAMP
+const changeTimestamp = (data) => {
+	const tanggal = new Date(data);
+	const tgl = tanggal.getDate();
+	const bln = tanggal.getMonth();
+	const thn = tanggal.getFullYear();
+	const dataBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+	const bulan = dataBulan[bln];
+
+	return tgl + " " + bulan + " " + thn;
+}
+// END TIMESTAMP
+
+judulGaleri.addEventListener("change", e => {
+	dataInputAdmin = {
+		...dataInputAdmin,
+		judul: e.target.value
+	}
+})
+
+const addGaleri = (data) => {
+	return new Promise((resolve, reject) => {
+		const db = getFirestore(app);
+		addDoc(collection(db, "Galeri"), data)
+			.then(() => {
+				console.log("succes !!!")
+				resolve(true) //TRIGGER SWEET ALERT SUCCES
+			})
+	});
+};
+
+btnGaleri.addEventListener("click", async () => {
+	fileItem = input_img.files[0];
+	dataInputAdmin = {
+		...dataInputAdmin,
+		url_img: fileItem
+	}
+	const { judul, url_img } =dataInputAdmin;
+	if (judul== "" || url_img== undefined ) {
+		Swal.fire({
+			icon: 'error',
+			title: 'Oops...',
+			text: 'Data tidak boleh kosong !!!',
+		})
+	}
+	else {
+		console.log(dataInputAdmin)
+		getFile()
+	}
+})
+// END INPUT BERITA SEKOLAH
+
+
+// TAMPIL BERITA SEKOLAH
+// DATA TABEL
+let dataGaleri = [];
+
+const getAllGaleri = () => {
+	return new Promise((resolve, reject) => {
+		const db = getFirestore(app);
+		getDocs(collection(db, "Galeri"))
+			.then(querySnapshot => {
+				querySnapshot.forEach((doc) => {
+					dataGaleri.push({
+						...doc.data(),
+						id: doc.id
+					})
+					// if(dataGaleri.length <= 5){
+					// 	beritaSekolah.innerHTML += addElGaleri(doc.data(), doc.id)
+					// }
+				});
+				console.log(dataGaleri)
+				$(document).ready(function () {
+					$('#table1').DataTable({
+						lengthMenu: [
+							[5, 8, 10],
+							[5, 8, 10],
+						],
+						scrollY: false,
+						destroy: true,
+						data: dataGaleri,
+						columnDefs: [{
+							"defaultContent": "-",
+							"targets": "_all"
+						}],
+						columns: [
+							{
+								render: function (data, type, JsonResultRow, meta) {
+									return '<img src="' + JsonResultRow.url_img + '"/>'
+								}
+							},
+							{ data: 'judul' },
+							{
+								render: function (data, type, JsonResultRow, meta) {
+									return changeTimestamp(JsonResultRow.tgl_uploud)
+								}
+							},
+							{
+								render: function (data, type, JsonResultRow, meta) {
+									return `
+									<button title="Edit" class="editData" data-bs-toggle="modal" id=${JsonResultRow.id} data-bs-target="#staticBackdrop"><i id=${JsonResultRow.id} class="fas fa-edit"></i></button>
+									<button class="btnDeleteId" id=${JsonResultRow.id} data-bs-toggle="modal" data-bs-target="#modalHapus" title="Hapus"><i class="fas fa-trash"></i></button>
+									`
+								}
+							}
+						],
+					});
+				});
+			})
+			.catch((error) => {
+				reject(error)
+			});
+	})
+}
+getAllGaleri()
+//  END DATA TABEL
+
+
+// BUTTON ACTION
+window.addEventListener("click", async (e) => {
+	if (e.target.classList.value == "btnDelete btn-primary") {
+		const id = localStorage.getItem("idDel")
+		const db = getFirestore(app);
+		await deleteDoc(doc(db, "Galeri", id));
+		localStorage.removeItem("idDel")
+		location.reload()
+	} else if (e.target.classList.value == "btnDeleteId") {
+		localStorage.setItem("idDel", e.target.id)
+	}
+})
+//  END BUTTON ACTION
+// END TAMPIL BERITA SEKOLAH
